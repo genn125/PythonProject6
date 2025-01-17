a65d738c391d6a321f93e55f40637ea050f9d6d6
genn125 <genn125@gmail.com>
2025-01-17 17:13:57 +0300

    1

---
+++ 6.4.py
@@ -0,0 +1,45 @@
+# 6.4 Наследование классов (hard)
+# Задание "Они все так похожи":
+
+class Figure:
+    sides_count = 0
+    def __init__ (self, __sides, __color, filled):
+
+    def get_color(self):
+
+
+    def __is_valid_color(self, r, g, b):
+
+    def set_colo(self, r, g, b):
+
+    def __is_valid_sides(self, *kwarg):
+
+    def get_sides(self):
+        return __sides
+
+    def __len__(self):
+        return
+    def set_sides(self, *new_sides):
+        if new_sides != sides_count:
+            new_sides = sides_count
+        else:
+            return
+
+
+class Circle (Figure):
+    sides_count = 1
+
+    def __radius(self):
+
+    def get_square
+
+
+
+
+
+class Triangle(Figure):
+
+
+class Cube (Figure):
+
+
